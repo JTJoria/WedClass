62040c1006bea64a7739a5e196b87e540b48d9b2
TIM JORIA <jtjoria@gmail.com>
2016-01-07 17:47:41 -0600

    endday4

--- creditcard.rb
+++ creditcard.rb
@@ -4,19 +4,26 @@
 
 class CreditCard
 
-  attr_accessor :credit_card
+  attr_accessor :account_number, :credit_card
 
   def initialize
-    @acct_num = []
+    @account_number = []
   end
 
-  def acct_num
-    acct_num = Array.new(10){rand(10)}.join("")
+  def account_number
+    @account_number = Array.new(10){rand(10)}.join("")
   end
 
+  def display_account_number
+    @account_number.pop (4)
+
+  end
 end
 
-p acct_num
+credit_card = CreditCard.new
+
+p credit_card.account_number
+p credit_card.display_account_number
 
 
 
